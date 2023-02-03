c52ca920a94b9422370f71f666fc0445d4afc637
Om Bhatnagar <omishb2001@gmail.com>
2023-02-03 16:28:03 +0530

    Added getstudentbyid, delete and update services for student.

--- day-3/practice/src/main/java/com/example/practice/service/StudentService.java
+++ day-3/practice/src/main/java/com/example/practice/service/StudentService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -14,7 +15,6 @@ public class StudentService {
     private StudentRepo studentRepo;
 
     //Create New student details
-
     public String createStudent(Student student){
         if(this.studentRepo.existsById(student.getId())){
             return "Student already exists";
@@ -26,16 +26,42 @@ public class StudentService {
     }
 
     //get all student details
-
     public List<Student> getStudents(){
         return this.studentRepo.findAll();
 
     }
 
     //delete student
+    public String deleteStudent(Integer id){
+        try{
+            if(this.studentRepo.existsById(id)){
+                this.studentRepo.deleteById(id);
+                return "Student record deleted.";
+            }else {
+                throw new Exception("Student does not exists!");
+            }
+        } catch(Exception e){
+            return "Error deleting student.";
+        }
+    }
 
     //update student
+    public String updateStudent(Student student){
+        try{
+            if(this.studentRepo.existsById(student.getId())){
+                this.studentRepo.save(student);
+                return "Updated successfully.";
+            } else{
+                throw new Exception("Student not found!");
+            }
+        }catch(Exception e){
+            return "Error updating!";
+        }
+    }
 
     //get student by id
+    public Optional<Student> getStudentById(Integer id){
+        return this.studentRepo.findById(id);
+    }
 
 }

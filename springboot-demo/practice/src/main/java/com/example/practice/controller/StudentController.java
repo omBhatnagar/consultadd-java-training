64b180595c25fc03e303e6efbd08272438bb44f5
Om Bhatnagar <omishb2001@gmail.com>
2023-02-10 18:13:39 +0530

    Removed unused import.

--- springboot-demo/practice/src/main/java/com/example/practice/controller/StudentController.java
+++ springboot-demo/practice/src/main/java/com/example/practice/controller/StudentController.java
@@ -4,7 +4,6 @@ import com.example.practice.model.Student;
 import com.example.practice.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
